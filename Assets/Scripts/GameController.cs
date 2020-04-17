7bc4fd44004f09261c58b84659318d3bc1891eab
jaygittings <jaygittings@yahoo.com>
2020-04-17 09:02:35 -0400

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    int guess;
+
+    [SerializeField] TextMeshProUGUI numberText;
+    [SerializeField] int minNumber;
+    [SerializeField] int maxNumber;
+    [SerializeField] Text instructions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instructions.text = string.Format("Please guess a number between {0} and {1}", minNumber, maxNumber);
+        //maxNumber++;
+        guess = NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        minNumber = guess + 1 < maxNumber ? guess + 1 : maxNumber;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        maxNumber = guess - 1 < minNumber ? minNumber : guess - 1;
+        NextGuess();
+    }
+
+    int NextGuess()
+    {
+        guess = Random.Range(minNumber, maxNumber + 1);
+        numberText.text = guess.ToString();
+        return guess;
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void NextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(SceneManager.sceneCountInBuildSettings);
+
+        if (currentSceneIndex + 1 < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1, LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
